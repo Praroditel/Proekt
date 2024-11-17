a692c746e9a7d8cbec9157d87f093141ba63f6f2
Praroditel <abalduyevnik@gmail.com>
2024-11-18 00:04:17 +0400

    Add files via upload

--- да/character_body_2d.gd
+++ да/character_body_2d.gd
@@ -4,12 +4,27 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -350.0
 var anim2 = true
+var рывок = 4000
+var исп_рывок = true
+var rivok_direction = Vector2.ZERO
+
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
+	var direction2 = Input.get_axis("ui_left", "ui_right")	
+	if Input.is_action_pressed("ui_left"):
+		rivok_direction = Vector2.LEFT
+		velocity.x = direction2 * рывок
+		
+	elif Input.is_action_pressed("ui_right"):
+		rivok_direction = Vector2.RIGHT
+		velocity.x = direction2 * рывок
+		
+	
+
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,9 +43,18 @@ func _physics_process(delta: float) -> void:
 	if anim2:
 		anim.play("Стою")	
 	if direction == -1:
-			$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
-		
-
+	dach()
 	move_and_slide()
+	
+func dach():#рывок
+	if Input.is_action_just_pressed("shift") and исп_рывок:
+		velocity = rivok_direction.normalized() * 2000
+		исп_рывок = false 
+		await get_tree().create_timer(2).timeout
+		исп_рывок = true
+	 
+		
+		
