ed67c2796ad2e258a4357d8f5009dab2494a6859
Praroditel <abalduyevnik@gmail.com>
2024-11-20 20:51:51 +0400

    Add files via upload

--- да/character_body_2d.gd
+++ да/character_body_2d.gd
@@ -4,60 +4,39 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -350.0
 var anim2 = true
-var рывок = 20000
+var рывок = 2000
 var исп_рывок = true
 var rivok_direction = Vector2.ZERO
-var TIMER = false
-			
-func _ready() -> void:
-	var TIMER2 = get_node("Timer")
-	TIMER2.timeout.connect(_on_timer_timeout)
+
+
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	var direction = Input.get_axis("ui_left", "ui_right")	
-	
-	
-	
-	
+	var direction2 = Input.get_axis("ui_left", "ui_right")	
+	if Input.is_action_pressed("ui_left"):
+		rivok_direction = Vector2.LEFT
+		velocity.x = direction2 * рывок
+		
+	elif Input.is_action_pressed("ui_right"):
+		rivok_direction = Vector2.RIGHT 
+		velocity.x = direction2 * рывок
+
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 	
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 		anim.play("Ходьба")
 		anim2 = false
-		if direction == -1 and Input.is_action_pressed("shift") and исп_рывок == true:
-			rivok_direction = Vector2.LEFT
-			velocity.x = direction * рывок 
-			исп_рывок = false
-			if TIMER == false:
-				$Timer.wait_time=1
-				$Timer.start()
-				TIMER = true
-			#if  исп_рывок == false:
-			#	get_tree().create_timer(2).timeout
-				#исп_рывок = true
-			   
-			  
-			   
-			
-		if direction == 1 and Input.is_action_pressed("shift") and исп_рывок:
-			rivok_direction = Vector2.RIGHT
-			velocity.x = direction * рывок 
-			исп_рывок = false
-			if TIMER == false:
-				$Timer.wait_time=3
-				$Timer.start()
-				TIMER = true
-			#await get_tree().create_timer(2).timeout
-			#исп_рывок = true
-		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		anim2= true
+		
 		
 	if anim2:
 		anim.play("Стою")	
@@ -65,15 +44,16 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
+	dach()
 	move_and_slide()
-func _on_timer_timeout() -> void:
-	if TIMER == true:
-	#	исп_рывок = true
-		print("GG")
-		исп_рывок = true
-		TIMER = false
-
 	
-	#if исп_рывок == false:
-	#	исп_рывок = true
-	#	print("GG")
+func dach():#рывок
+	if Input.is_action_just_pressed("shift") and исп_рывок :
+		velocity = rivok_direction.normalized() * 3000
+		исп_рывок = false 
+		await get_tree().create_timer(2).timeout
+		print("5")
+		исп_рывок = true
+	
+		
+		
